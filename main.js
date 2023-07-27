8a17b0e1402540aecb4caf48d2455295fd5eb645
Dean Patterson <deanp04@gmail.com>
2023-07-27 01:28:08 -0400

    updated modals to populate correctly

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
-document.querySelector('button').addEventListener('click', apiRequest)
-
-async function apiRequest(){
-    const charName = document.querySelector('input').value
-    try{
-        const response = await fetch(`https://star-wars-api-w4mc.onrender.com/api/${charName}`)
-        const data = await response.json()
-
-        document.querySelector('.homeworld').innerText = 'homeworld: ' + data.homeworld
-        document.querySelector('.species').innerText = 'species: ' + data.species
-        document.querySelector('.height').innerText = 'height: ' + data.height
-        document.querySelector('.allegiance').innerText = 'allegiance: ' + data.allegiance
-        const characterImage = document.querySelector('.character-image');
-        characterImage.src = data.image;
-    }catch(error){
-        console.log(error)
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', function() {
+      apiRequest(this.textContent);
+    });
+  });
+  
+  async function apiRequest(charName) {
+    try {
+      const response = await fetch(`https://star-wars-api-w4mc.onrender.com/api/${charName}`);
+      const data = await response.json();
+  
+      // Find the modal element dynamically
+      const modal = document.getElementById('modal1');
+      
+      // Populate modal elements with the retrieved information
+      modal.querySelector('.font-bold').innerText = data.name;
+      modal.querySelector('.homeworld').innerText = 'Homeworld: ' + data.homeworld;
+      modal.querySelector('.species').innerText = 'Species: ' + data.species;
+      modal.querySelector('.height').innerText = 'Height: ' + data.height;
+      modal.querySelector('.allegiance').innerText = 'Allegiance: ' + data.allegiance;
+  
+      // Set the image source (URL) and alt attribute to the character's image URL and name, respectively
+      const characterImage = modal.querySelector('.character-image');
+      characterImage.src = data.image; // Assuming the API response contains the image URL as 'image'
+      characterImage.alt = charName;
+  
+      // Show the modal
+      modal.showModal();
+    } catch (error) {
+      console.error('Error fetching character information:', error);
     }
-}
+  }
+  
+  // Function to close the modal
+  function closeModal() {
+    const modal = document.getElementById('modal1');
+    modal.close();
+  }
